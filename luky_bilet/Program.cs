cf29e58336553e054412f07af87df3ca880a4d9c
arina10006 <arina1006@mail.ru>
2017-02-22 11:35:32 +0600

    create luky tiket

---
+++ luky_bilet/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luky_bilet
+{
+    class Program
+    {
+        static int SizeOfNumber(int number)
+        {
+            int size = 0;
+            while (number > 0 )
+            {
+                number = number / 10;
+                size++;
+            }
+            return size;
+        }
+
+        static int SummLeftOfNumber(int number, int halfOfSize)
+        {
+            int summLeftRight =0;
+            int a = number / (int)Math.Pow(10, halfOfSize);
+            while (a > 0)
+            {
+                summLeftRight = summLeftRight + a % 10;
+                a = a / 10;
+            }
+            return summLeftRight;
+        }
+
+        static int SummRightOfNumber(int number, int halfOfSize, int size)
+        {
+            int summRight = 0;
+            if (size % 2 == 0)
+            {
+                int a = number % (int)Math.Pow(10, halfOfSize);
+                while (a > 0)
+                {
+                    summRight = summRight + a % 10;
+                    a = a / 10;
+                }
+
+            }
+            else
+            {
+                int a = number % (int)Math.Pow(10, halfOfSize +1) ;
+                while (a > 0)
+                {
+                    summRight = summRight + a % 10;
+                    a = a / 10;
+                }
+
+                
+            }
+            return summRight;
+        }
+
+        static bool ComparisonOfTheTwoHalves(int summLeft, int summRight)
+        {
+            bool compare = summLeft == summRight;
+
+            return compare;
+        }
+
+        static void Main(string[] args)
+        {
+            int number =9874786;
+
+            int size = SizeOfNumber(number);
+
+            Console.WriteLine("size = {0}", size);
+
+            Console.WriteLine("number = {0}", number);
+            int halfOfSize = size / 2;
+            Console.WriteLine("halfOfSize = {0}", halfOfSize);
+
+            int summLeft = SummLeftOfNumber(number, halfOfSize);
+
+            Console.WriteLine("summLeft = {0}", summLeft);
+
+            int summRight = SummRightOfNumber(number, halfOfSize, size);
+
+            Console.WriteLine("summRight = {0}", summRight);
+
+            bool compare = ComparisonOfTheTwoHalves(summLeft, summRight);
+            if (compare)
+            {
+                Console.WriteLine("Билетик счастливый");
+            }
+            else
+            {
+                Console.WriteLine("Билетик не счастливый");
+            }
+            
+        }
+    }
+}
---
+++ luky_bilet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("luky_bilet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("luky_bilet")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bc9c2c5d-0335-4ffb-b896-ec0530111c7e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
